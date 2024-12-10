1a89c8b0dac221701df16b2468bf9c72b57da81e
jesus <jerola_1986@hotmail.com>
2024-12-10 19:16:07 +0100

    Paso 1 Formulario recogida numeros

---
+++ calculadora.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+    <h1>Calculadora Básica</h1>
+
+    <form action="#"method="POST">
+
+    <label for="numero1">Introduce el primer numero: </label>
+    <input type="text" name="numero1"><br>
+
+    <label for="numero2">Introduce el segundo numero: </label>
+    <input type="text" name="numero2">
+
+    <input type="submit" value="Enviar">
+    </form>
+
+</body>
+</html>
